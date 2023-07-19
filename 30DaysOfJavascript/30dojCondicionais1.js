ee8bd79be3ed4e88345d2af5c9a14600ca52640e
lucas-jorge <21230436+lucas-jorge@users.noreply.github.com>
2023-07-19 15:00:55 -0300

    Add files via upload

---
+++ 30DaysOfJavascript/30dojCondicionais1.js
@@ -0,0 +1,12 @@
+var idade = 16;
+console.log(idade.prompt);
+
+if (idade >= 18){
+
+    console.log('Você é maior de idade.')
+
+} else {
+
+    console.log(`Você é menor de idade e faltam &{18 - idade} anos para a maioridade!`)
+
+};
---
+++ 30DaysOfJavascript/30dojCondicionais2.js
@@ -0,0 +1,17 @@
+let minhaIdade = 36;
+let suaIdade = 27;
+let diff = minhaIdade - suaIdade;
+
+if (diff > 0) {
+
+    console.log(`Eu sou ${diff} anos mais velho do que você.`)
+
+} else if (diff < 0) {
+
+    console.log(`Você é ${Math.abs(diff)} anos mais velho do que eu.`)
+
+} else {
+
+    console.log('Você é da minha idade.')
+
+};
